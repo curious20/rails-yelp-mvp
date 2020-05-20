5b0f57ed4a1a22bc65a41db820558b38f4e269fe
leo huettig <learntocode2020@protonmail.com>
2020-05-20 15:47:01 +0200

    seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+restaurant_categories = %W[chinese italian japanese french belgian]
+10.times do
+  @new_restautrant = Restaurant.create(
+    name: Faker::Name.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::Number.number(digits: 10),
+    category: restaurant_categories[rand(restaurant_categories.length)]
+  )
+  rand(2..10).times do
+    Review.create(
+      content: Faker::Hipster.paragraphs(number: 1),
+      rating: rand(0..5),
+      restaurant: @new_restautrant
+    )
+  end
+end
