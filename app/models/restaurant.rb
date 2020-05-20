1668bcd284416b5430220219ec6777df3afffc4c
leo huettig <learntocode2020@protonmail.com>
2020-05-20 16:36:56 +0200

    stars and raiting :D

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -7,4 +7,14 @@ class Restaurant < ApplicationRecord
   validates :name, :address, :category, presence: true
   validates :category, inclusion: { in: allowed_categories }
   validates :phone_number, presence: true, format: { with: /[\d]/ }
+
+  def rating
+    count = reviews.count
+    'no ratings yet' if count.zero?
+    sum = 0
+    reviews.each do |review|
+      sum += review.rating
+    end
+    (sum / count.to_f).round(2)
+  end
 end
