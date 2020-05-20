548788c0045f94743419be39097fdcd9902431dc
leo huettig <learntocode2020@protonmail.com>
2020-05-20 15:04:18 +0200

    Validation added

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,10 @@
 class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
+
+  def self.allowed_categories
+    %W[chinese italian japanese french belgian]
+  end
+  validates :name, :address, :category, presence: true
+  validates :category, inclusion: { in: allowed_categories }
+  validates :phone_number, presence: true, format: { with: /[\d]/ }
 end
